6562dfef448b631873cb5e7be8dfed1fc3b9af60
Daniel Mills <danielmillst@gmail.com>
2016-10-17 19:33:34 -0400

    Main class

---
+++ src/main/java/org/cyberpwn/saron/Saron.java
@@ -0,0 +1,30 @@
+package org.cyberpwn.saron;
+
+import org.phantomapi.construct.Ghost;
+
+public class Saron extends Ghost
+{
+	@Override
+	public void preStart()
+	{
+		
+	}
+	
+	@Override
+	public void onStart()
+	{
+		
+	}
+	
+	@Override
+	public void onStop()
+	{
+		
+	}
+	
+	@Override
+	public void postStop()
+	{
+		
+	}
+}
