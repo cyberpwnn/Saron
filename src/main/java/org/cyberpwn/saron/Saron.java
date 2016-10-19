e176efb6b698d882170721632c51af2d6bfeb207
Daniel Mills <danielmillst@gmail.com>
2016-10-19 05:14:01 -0400

    Register instance mgr

--- src/main/java/org/cyberpwn/saron/Saron.java
+++ src/main/java/org/cyberpwn/saron/Saron.java
@@ -2,12 +2,21 @@ package org.cyberpwn.saron;
 
 import org.phantomapi.construct.Ghost;
 
+/**
+ * Base saron object
+ * 
+ * @author cyberpwn
+ */
 public class Saron extends Ghost
 {
+	private InstanceManager instanceManager;
+	
 	@Override
 	public void preStart()
 	{
+		instanceManager = new InstanceManager(this);
 		
+		register(instanceManager);
 	}
 	
 	@Override
