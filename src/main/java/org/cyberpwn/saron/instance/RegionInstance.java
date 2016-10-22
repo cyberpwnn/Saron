c28a89a6b469b537f1a8e06f26f60f8934b14861
Daniel Mills <danielmillst@gmail.com>
2016-10-22 17:45:27 -0400

    region config

--- src/main/java/org/cyberpwn/saron/instance/RegionInstance.java
+++ src/main/java/org/cyberpwn/saron/instance/RegionInstance.java
@@ -1,7 +1,19 @@
 package org.cyberpwn.saron.instance;
 
+import org.phantomapi.clust.Comment;
+import org.phantomapi.clust.Keyed;
+import org.phantomapi.lang.GList;
+
 public abstract class RegionInstance extends BaseInstance
 {
+	@Comment("The point locations for this region")
+	@Keyed("region.points")
+	public GList<String> points = new GList<String>();
+	
+	@Comment("The name of this region")
+	@Keyed("region.name")
+	public String name = "&aRegion";
+	
 	public RegionInstance(String type)
 	{
 		super(type);
