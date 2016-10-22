ef07df2f2c15a1b42eee271e8a8b8eea9e37ed65
Daniel Mills <danielmillst@gmail.com>
2016-10-22 17:45:22 -0400

    add the spawn instance

--- src/main/java/org/cyberpwn/saron/instance/SpawnInstance.java
+++ src/main/java/org/cyberpwn/saron/instance/SpawnInstance.java
@@ -1,7 +1,18 @@
 package org.cyberpwn.saron.instance;
 
+import org.phantomapi.clust.Comment;
+import org.phantomapi.clust.Keyed;
+
 public abstract class SpawnInstance extends RegionInstance
 {
+	@Comment("The type of instance to spawn in this region")
+	@Keyed("region.spawn.type")
+	public String spawnType = "NONE";
+	
+	@Comment("The max instances to keep spawned in this region")
+	@Keyed("region.spawn.max-instances")
+	public int maxInstances = 24;
+	
 	public SpawnInstance(String type)
 	{
 		super(type);
