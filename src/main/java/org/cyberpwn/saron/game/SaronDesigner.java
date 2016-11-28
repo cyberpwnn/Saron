f82c2a20bf93b95f00b302be76b437faceac038e
Daniel Mills <danielmillst@gmail.com>
2016-11-28 12:28:29 -0500

    Saron designer game

---
+++ src/main/java/org/cyberpwn/saron/game/SaronDesigner.java
@@ -0,0 +1,36 @@
+package org.cyberpwn.saron.game;
+
+import org.bukkit.entity.Player;
+import org.phantomapi.game.GamePlugin;
+
+public class SaronDesigner extends SaronGame
+{
+	public SaronDesigner(GamePlugin plugin)
+	{
+		super(plugin);
+	}
+	
+	@Override
+	public void onStart(String... lauchParameters)
+	{
+		
+	}
+	
+	@Override
+	public void onStop(String... endParameters)
+	{
+		
+	}
+	
+	@Override
+	public void onJoin(Player p)
+	{
+		
+	}
+	
+	@Override
+	public void onQuit(Player p)
+	{
+		
+	}
+}
