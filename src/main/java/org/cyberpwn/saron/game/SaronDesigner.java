2320be27c7ab11d8fd4386c5e9220031725bacf9
Daniel Mills <danielmillst@gmail.com>
2016-11-28 13:01:48 -0500

    Designer fixes

--- src/main/java/org/cyberpwn/saron/game/SaronDesigner.java
+++ src/main/java/org/cyberpwn/saron/game/SaronDesigner.java
@@ -5,13 +5,17 @@ import org.cyberpwn.saron.CFG;
 import org.phantomapi.game.CapacityMode;
 import org.phantomapi.game.GamePlugin;
 import org.phantomapi.game.ResourcePackMode;
+import org.phantomapi.sync.Task;
 
 public class SaronDesigner extends SaronGame
 {
+	private Task task;
+	
 	public SaronDesigner(GamePlugin plugin)
 	{
 		super(plugin);
 		
+		task = null;
 		getProfile().getCapacityProfile().setMode(CapacityMode.valueOf(CFG.i.designerCapacityMode));
 		getProfile().getCapacityProfile().setAcceptingPlayers(CFG.i.designerEnabled);
 		getProfile().getResourceProfile().setMode(ResourcePackMode.NONE);
@@ -20,13 +24,20 @@ public class SaronDesigner extends SaronGame
 	@Override
 	public void onStart(String... lauchParameters)
 	{
-		
+		task = new Task(0)
+		{
+			@Override
+			public void run()
+			{
+				
+			}
+		};
 	}
 	
 	@Override
 	public void onStop(String... endParameters)
 	{
-		
+		task.cancel();
 	}
 	
 	@Override
