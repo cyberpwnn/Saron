5744b64a6ba494fc46a4d3c19abf4ee1ae05b83b
Daniel Mills <danielmillst@gmail.com>
2016-11-28 12:27:36 -0500

    Tweak saron

--- src/main/java/org/cyberpwn/saron/Saron.java
+++ src/main/java/org/cyberpwn/saron/Saron.java
@@ -1,18 +1,28 @@
 package org.cyberpwn.saron;
 
 import org.phantomapi.construct.Ghost;
+import org.phantomapi.game.GamePlugin;
 
 /**
  * Base saron object
  * 
  * @author cyberpwn
  */
-public class Saron extends Ghost
+public class Saron extends Ghost implements GamePlugin
 {
+	public static Saron instance;
+	private CFG cfg;
+	private SaronInstance saronInstance;
+	
 	@Override
 	public void preStart()
 	{
+		instance = this;
+		cfg = new CFG();
+		loadCluster(cfg);
+		saronInstance = new SaronInstance(this);
 		
+		register(saronInstance);
 	}
 	
 	@Override
@@ -31,5 +41,20 @@ public class Saron extends Ghost
 	public void postStop()
 	{
 		
+	}
+	
+	public static Saron getInstance()
+	{
+		return instance;
+	}
+	
+	public CFG getCfg()
+	{
+		return cfg;
+	}
+	
+	public SaronInstance getSaronInstance()
+	{
+		return saronInstance;
 	}
 }
