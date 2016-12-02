1e5cbf8378e4c29e3c0b4c6507f8d60cc7f0611e
Daniel Mills <danielmillst@gmail.com>
2016-12-01 23:49:35 -0500

    CFG

--- src/main/java/org/cyberpwn/saron/CFG.java
+++ src/main/java/org/cyberpwn/saron/CFG.java
@@ -2,10 +2,14 @@ package org.cyberpwn.saron;
 
 import org.phantomapi.clust.ConfigurableObject;
 import org.phantomapi.clust.DataCluster;
+import org.phantomapi.clust.Keyed;
 
 public class CFG extends ConfigurableObject
 {
 	public static CFG i;
+	
+	@Keyed("game.enabled")
+	public boolean game = false;
 	
 	public CFG()
 	{
