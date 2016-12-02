fe0cb28a4af61577dd2479e975f962b5c31d5048
Daniel Mills <danielmillst@gmail.com>
2016-12-01 23:05:46 -0500

    Edit cfg

--- src/main/java/org/cyberpwn/saron/CFG.java
+++ src/main/java/org/cyberpwn/saron/CFG.java
@@ -1,31 +1,17 @@
 package org.cyberpwn.saron;
 
-import org.bukkit.entity.Player;
 import org.phantomapi.clust.ConfigurableObject;
 import org.phantomapi.clust.DataCluster;
-import org.phantomapi.clust.Keyed;
-import org.phantomapi.text.MessageBuilder;
 
 public class CFG extends ConfigurableObject
 {
 	public static CFG i;
-	
-	@Keyed("designer.enabled")
-	public boolean designerEnabled = true;
-	
-	@Keyed("designer.capacity-mode")
-	public String designerCapacityMode = "UNLIMITED";
 	
 	public CFG()
 	{
 		super("configuration");
 		
 		i = this;
-	}
-	
-	public static void msg(Player p, String msg)
-	{
-		new MessageBuilder(Saron.instance.getSaronInstance()).message(p, msg);
 	}
 	
 	public static DataCluster get()
